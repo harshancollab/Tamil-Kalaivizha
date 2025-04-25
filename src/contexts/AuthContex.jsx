896d875785f049bb4bf2709433059517f8130ee0
harshancollab <harshan.collabtechnosoftwares@gmail.com>
2025-04-25 21:48:20 +0530

    v1

--- src/contexts/AuthContex.jsx
+++ src/contexts/AuthContex.jsx
@@ -65,7 +65,7 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   // Simulating a logged-in sub district admin user (for testing)
-  const [user, setUser] = useState({ role: 'sub district admin', isAuthenticated: true });
+  const [user, setUser] = useState({ role: 'It Admin', isAuthenticated: true });
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
